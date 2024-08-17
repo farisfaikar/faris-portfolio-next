5a486af02a0fb5b59f45ca37554a826c35f0fadf
Faris Faikar <farisfaikar.r@gmail.com>
2024-08-17 23:45:27 +0700

    hotfix: add break-words to article tag in blog detail

--- src/app/blog/[slug]/page.tsx
+++ src/app/blog/[slug]/page.tsx
@@ -89,7 +89,7 @@ export default async function Blog({
         </Suspense>
       </div>
       <article
-        className="prose dark:prose-invert"
+        className="prose dark:prose-invert break-words"
         dangerouslySetInnerHTML={{ __html: post.source }}
       ></article>
     </section>
