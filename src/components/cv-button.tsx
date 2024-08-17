dd98af54339e365628a7a3c1a42040e4532819b2
Faris Faikar <farisfaikar.r@gmail.com>
2024-08-17 22:54:46 +0700

    remove: shiny motion effect from cv-button

--- src/components/cv-button.tsx
+++ src/components/cv-button.tsx
@@ -67,17 +67,6 @@ const CVButton: React.FC<CVButtonProps> = ({ targetText }) => {
                 <IoMdDownload />
                 <span>{text}</span>
             </div>
-            <motion.span
-                initial={{ y: "100%" }}
-                animate={{ y: "-100%" }}
-                transition={{
-                    repeat: Infinity,
-                    repeatType: "mirror",
-                    duration: 1,
-                    ease: "linear",
-                }}
-                className="duration-300 absolute inset-0 z-0 scale-125 bg-gradient-to-t from-blue-400/0 from-40% dark:via-blue-400/100 to-blue-400/0 to-60% opacity-0 transition-opacity group-hover:opacity-100"
-            />
         </motion.button>
     );
 };
