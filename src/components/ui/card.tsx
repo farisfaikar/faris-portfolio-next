2ea548576ea094b1eafd1a6d23c91fdd823c13a2
Faris Faikar <farisfaikar.r@gmail.com>
2024-08-17 22:55:44 +0700

    fix: remove card background color to make it transparent

--- src/components/ui/card.tsx
+++ src/components/ui/card.tsx
@@ -6,7 +6,7 @@ const Card = React.forwardRef<HTMLDivElement, React.HTMLAttributes<HTMLDivElemen
   ({ className, ...props }, ref) => (
     <div
       ref={ref}
-      className={cn("rounded-lg bg-card text-card-foreground", className)}
+      className={cn("rounded-lg text-card-foreground", className)}
       {...props}
     />
   ),
