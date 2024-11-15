93dff45acd3dafa02e97b86e82691cfb66f370fb
Faris Faikar <farisfaikar.r@gmail.com>
2024-11-15 08:57:46 +0700

    hotfix: add images to metadata in layout.tsx

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -28,6 +28,14 @@ export const metadata: Metadata = {
     siteName: `${DATA.name}`,
     locale: "en_US",
     type: "website",
+    images: [
+      {
+        url: '/opengraph-image.png',
+        width: 1200,
+        height: 630,
+        alt: `${DATA.name} - Open Graph Image`
+      }
+    ]
   },
   robots: {
     index: true,
@@ -43,6 +51,14 @@ export const metadata: Metadata = {
   twitter: {
     title: `${DATA.name}`,
     card: "summary_large_image",
+    images: [
+      {
+        url: '/twitter-image.png',
+        width: 1200,
+        height: 630,
+        alt: `${DATA.name} - Twitter Card Image`
+      }
+    ]
   },
   verification: {
     google: "",
