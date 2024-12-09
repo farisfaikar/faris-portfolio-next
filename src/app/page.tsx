96efa8f13f47b5cd8cbd2897a5b95a5a00a764bc
Faris Faikar <farisfaikar.r@gmail.com>
2024-12-09 23:29:41 +0700

    perf: set fetchPriority to high for profile image on hero page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -47,7 +47,7 @@ export default function Page() {
             </div>
             <BlurFade delay={BLUR_FADE_DELAY}>
               <Avatar className="size-28 border">
-                <AvatarImage alt={DATA.name} src={DATA.avatarUrl} />
+                <AvatarImage alt={DATA.name} src={DATA.avatarUrl} fetchPriority="high" />
                 <AvatarFallback>{DATA.initials}</AvatarFallback>
               </Avatar>
             </BlurFade>
