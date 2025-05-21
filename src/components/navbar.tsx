43f1e06a2f4805ee5c98dfb65ae8400b99786774
Faris Faikar <farisfaikar.r@gmail.com>
2025-05-21 19:10:47 +0700

    fix: increase min-w from 32 to 40 on navbar
    note: this is to remove jitters on navbar when time-display is running

--- src/components/navbar.tsx
+++ src/components/navbar.tsx
@@ -14,7 +14,7 @@ export default function Navbar() {
     <div className="pointer-events-none fixed inset-x-0 bottom-0 z-30 mx-auto my-4 flex h-full max-h-14 origin-bottom px-10 md:top-0">
       <div className="fixed inset-x-0 bottom-0 h-16 w-full bg-background to-transparent backdrop-blur-lg [-webkit-mask-image:linear-gradient(to_top,black,transparent)] dark:bg-background md:top-0 md:[-webkit-mask-image:linear-gradient(to_bottom,black,transparent)]"></div>
       <div className="mx-auto flex w-full max-w-7xl items-center justify-between">
-        <div className="hidden items-center justify-center md:flex">
+        <div className="hidden min-w-40 items-center justify-center md:flex">
           <Badge variant="secondary">Indonesia/Bandung</Badge>
         </div>
         <Dock className="pointer-events-auto relative z-50 mx-auto flex h-full min-h-full transform-gpu items-center bg-background px-1 [box-shadow:0_0_0_1px_rgba(0,0,0,.03),0_2px_4px_rgba(0,0,0,.05),0_12px_24px_rgba(0,0,0,.05)] dark:[border:1px_solid_rgba(255,255,255,.1)] dark:[box-shadow:0_-20px_80px_-20px_#ffffff1f_inset]">
@@ -66,7 +66,7 @@ export default function Navbar() {
             </Tooltip>
           </DockIcon>
         </Dock>
-        <div className="hidden min-w-32 items-center justify-end md:flex">
+        <div className="hidden min-w-40 items-center justify-end md:flex">
           <Badge variant="secondary">
             <TimeDisplay />
           </Badge>
