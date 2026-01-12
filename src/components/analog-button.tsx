df2e2afd2fe65715c898b883b46b4300544327f5
Faris Faikar <farisfaikar.r@gmail.com>
2026-01-12 22:00:38 +0700

    fix: analog button style

--- src/components/analog-button.tsx
+++ src/components/analog-button.tsx
@@ -18,19 +18,22 @@ export default function AnalogButton({
       onClick={goToLinkedin}
       className={`group relative cursor-pointer outline-none active:outline-none w-full sm:w-auto ${className}`}
     >
-      <span className="absolute top-0 left-0 h-full w-full rounded-lg transform translate-y-1 bg-neutral-950 dark:bg-neutral-400" />
+      <span className="absolute top-0 left-0 h-full w-full rounded-lg transform translate-y-1 bg-neutral-950 dark:bg-neutral-50" />
       
-      <span className="absolute top-0 left-0 h-full w-full rounded-lg transform translate-y-0.5 bg-neutral-950 dark:bg-neutral-400" />
+      <span className="absolute top-0 left-0 h-full w-full rounded-lg transform translate-y-0.5 bg-neutral-950 dark:bg-neutral-50" />
 
       <span className="
         relative block px-6 py-2 rounded-lg font-bold text-lg
         transition-transform duration-75 transform -translate-y-0.5
         group-hover:translate-y-0
         group-active:translate-y-0.5
-        text-white
-        bg-neutral-800
-        dark:text-black
-        dark:bg-neutral-50
+        border
+        text-neutral-950
+        bg-neutral-50
+        border-neutral-950
+        dark:text-neutral-50
+        dark:bg-neutral-950
+        dark:border-neutral-50
       ">
         {children}
       </span>
